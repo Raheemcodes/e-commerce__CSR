05155b001f150862af9e8cc4362267c13cbc2bca
Raheemcodes <raheemscorporatemail@gmail.com>
2024-05-04 09:01:55 +0100

    Implementation of image product view

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -1,34 +1,50 @@
 import { Product } from '../../utilities/products.utility';
+import Backdrop from '../backdrop/Backrop';
+import CloseImageBtn from '../icons/CloseImageBtn';
 import Navigation from '../icons/Navigation';
+import Thumbnail from '../thumbnail/Thumbnail';
 import classes from './Image.module.scss';
 
 const Image = ({ product }: { product: Product }): JSX.Element => {
   return (
-    <ul className={classes['list']}>
-      {product.images.map((image, index) => (
-        <li className={classes['item']} key={index}>
-          <img
-            width={1000}
-            height={1000}
-            src={image}
-            alt={product.title}
-            loading='lazy'
-          />
-        </li>
-      ))}
-      <button
-        type='button'
-        className={`${classes['navigation']} ${classes['left']}`}
-      >
-        <Navigation />
-      </button>
-      <button
-        type='button'
-        className={`${classes['navigation']} ${classes['right']}`}
-      >
-        <Navigation />
-      </button>
-    </ul>
+    <>
+      <div className={classes['container']}>
+        <button type='button' className={classes['close-btn']}>
+          <CloseImageBtn />
+        </button>
+
+        <ul className={classes['list']}>
+          {product.images.map((image, index) => (
+            <li className={classes['item']} key={index}>
+              <img
+                width={1000}
+                height={1000}
+                src={image}
+                alt={product.title}
+                loading='lazy'
+              />
+            </li>
+          ))}
+          <button
+            type='button'
+            className={`${classes['navigation']} ${classes['left']}`}
+          >
+            <Navigation />
+          </button>
+          <button
+            type='button'
+            className={`${classes['navigation']} ${classes['right']}`}
+          >
+            <Navigation />
+          </button>
+        </ul>
+
+        <div className={classes['thumbnail-container']}>
+          <Thumbnail product={product} />
+        </div>
+      </div>
+      <Backdrop duration={300} isOpen={true} onClick={() => {}} />
+    </>
   );
 };
 
--- src/components/thumbnail/Thumbnail.tsx
+++ src/components/thumbnail/Thumbnail.tsx
@@ -1,7 +1,22 @@
+import { Product } from '../../utilities/products.utility';
 import classes from './Thumbnail.module.scss';
 
-const Thumbnail = (): JSX.Element => {
-  return <ul className={classes['list']}></ul>;
+const Thumbnail = ({ product }: { product: Product }): JSX.Element => {
+  return (
+    <ul className={classes['list']}>
+      {product.thumbnails.map((thumbnail, idx) => (
+        <li className={classes['item']} key={idx}>
+          <img
+            width={176}
+            height={176}
+            src={thumbnail}
+            alt={`${product.title} ${idx + 1}`}
+            loading='lazy'
+          />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Thumbnail;
